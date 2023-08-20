b4a1283838b5d8d544666c2a15a4ac994acf825a
Sam <info@samwightwick.co.uk>
2023-08-20 23:15:24 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import logo from './images/logo.png';
 import { useEffect, useState } from 'react';
+import 'react-tooltip/dist/react-tooltip.css'
+import { Tooltip } from 'react-tooltip'
+
+
 
 function App() {
   const [seller, setSeller] = useState('0');
@@ -55,39 +59,57 @@ function App() {
           <h2 className='text-md mb-2.5 ml-3'>Seller fee calculator</h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-          <div className="flex flex-col rounded-xl p-5 bg-black border-darkGrey border no-controls">
-            <span className='mb-2 text-md md:text-xl'>Full ticket cost</span>
+          <div className="flex flex-col rounded-xl py-5 px-1 bg-black border-darkGrey border no-controls">
+            <div className='flex flex-row items-center justify-center mb-2'>
+              <span className='text-md md:text-xl'>Full cost per ticket</span>
+              <a data-tooltip-id="full-cost" data-tooltip-content="Full price paid for 1 ticket including any extra fees">
+                <svg class="w-4 h-4 ml-1 mt-[3px]" width="800px" height="800px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#666666" d="M512 64a448 448 0 110 896.064A448 448 0 01512 64zm67.2 275.072c33.28 0 60.288-23.104 60.288-57.344s-27.072-57.344-60.288-57.344c-33.28 0-60.16 23.104-60.16 57.344s26.88 57.344 60.16 57.344zM590.912 699.2c0-6.848 2.368-24.64 1.024-34.752l-52.608 60.544c-10.88 11.456-24.512 19.392-30.912 17.28a12.992 12.992 0 01-8.256-14.72l87.68-276.992c7.168-35.136-12.544-67.2-54.336-71.296-44.096 0-108.992 44.736-148.48 101.504 0 6.784-1.28 23.68.064 33.792l52.544-60.608c10.88-11.328 23.552-19.328 29.952-17.152a12.8 12.8 0 017.808 16.128L388.48 728.576c-10.048 32.256 8.96 63.872 55.04 71.04 67.84 0 107.904-43.648 147.456-100.416z"/></svg>
+              </a>
+              <Tooltip id="full-cost" />
+            </div>
             <input type="number" inputMode="decimal" onChange={handleTicketPrice} value={ticketPrice} className="flex relative text-4xl md:text-5xl md:leading-[3.5rem] text-center bg-transparent w-auto outline-none font-bold"/> 
           </div>
           <div className="flex flex-col rounded-xl p-5 bg-black border-darkGrey border no-controls">
-            <span className='mb-2 text-md md:text-xl'>Seller listing price</span>
+            <div className='flex flex-row items-center justify-center mb-2'>
+              <span className='text-md md:text-xl'>Your listing price</span>
+              <a data-tooltip-id="listing" data-tooltip-content="Price per ticket input box on the my listings page">
+                <svg class="w-4 h-4 ml-1 mt-[3px]" width="800px" height="800px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#666666" d="M512 64a448 448 0 110 896.064A448 448 0 01512 64zm67.2 275.072c33.28 0 60.288-23.104 60.288-57.344s-27.072-57.344-60.288-57.344c-33.28 0-60.16 23.104-60.16 57.344s26.88 57.344 60.16 57.344zM590.912 699.2c0-6.848 2.368-24.64 1.024-34.752l-52.608 60.544c-10.88 11.456-24.512 19.392-30.912 17.28a12.992 12.992 0 01-8.256-14.72l87.68-276.992c7.168-35.136-12.544-67.2-54.336-71.296-44.096 0-108.992 44.736-148.48 101.504 0 6.784-1.28 23.68.064 33.792l52.544-60.608c10.88-11.328 23.552-19.328 29.952-17.152a12.8 12.8 0 017.808 16.128L388.48 728.576c-10.048 32.256 8.96 63.872 55.04 71.04 67.84 0 107.904-43.648 147.456-100.416z"/></svg>
+              </a>
+              <Tooltip id="listing" />
+            </div>
             <input type="number" inputMode="decimal" onChange={handleSellerPrice} value={seller} className="flex relative text-4xl md:text-5xl md:leading-[3.5rem] text-center bg-transparent w-auto outline-none font-bold"/> 
           </div>
           <div className="flex flex-col rounded-xl p-5 bg-black border-darkGrey border">
-            <span className='mb-2 text-md md:text-xl'>Amount of tickets</span>
+            <div className='flex flex-row items-center justify-center mb-2'>
+              <span className='text-md md:text-xl'>Amount of tickets</span>
+              <a data-tooltip-id="amount" data-tooltip-content="The number of tickets you want to sell that cost THE SAME original purchase price">
+                <svg class="w-4 h-4 ml-1 mt-[3px]" width="800px" height="800px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#666666" d="M512 64a448 448 0 110 896.064A448 448 0 01512 64zm67.2 275.072c33.28 0 60.288-23.104 60.288-57.344s-27.072-57.344-60.288-57.344c-33.28 0-60.16 23.104-60.16 57.344s26.88 57.344 60.16 57.344zM590.912 699.2c0-6.848 2.368-24.64 1.024-34.752l-52.608 60.544c-10.88 11.456-24.512 19.392-30.912 17.28a12.992 12.992 0 01-8.256-14.72l87.68-276.992c7.168-35.136-12.544-67.2-54.336-71.296-44.096 0-108.992 44.736-148.48 101.504 0 6.784-1.28 23.68.064 33.792l52.544-60.608c10.88-11.328 23.552-19.328 29.952-17.152a12.8 12.8 0 017.808 16.128L388.48 728.576c-10.048 32.256 8.96 63.872 55.04 71.04 67.84 0 107.904-43.648 147.456-100.416z"/></svg>
+              </a>
+              <Tooltip id="amount" />
+            </div>
             <input type="number" inputMode="numeric" onChange={handleTicketAmount} value={ticketAmount}  min="1" className="flex relative text-4xl md:text-5xl md:leading-[3.5rem] text-center bg-transparent outline-none w-auto font-bold"/> 
           </div>
         </div>
         <div className="flex flex-col gap-y-5 justify-center mt-5">
           <div className="flex flex-col md:flex-row items-center justify-center border w-full p-3 rounded-xl border-lightGrey gap-x-1">
-            <span className='mb-0 text-xl text-sm md:text-lg'>Viagogo Listing price:</span>
-            <span className='mb-0 text-xl text-sm md:text-lg'><b>£{page} per ticket</b></span>
+            <span className='mb-0 text-xl md:text-lg'>Viagogo Listing price:</span>
+            <span className='mb-0 text-xl md:text-lg'><b>£{page} per ticket</b></span>
           </div>
           <div className="flex flex-col md:flex-row items-center justify-center border w-full p-3 rounded-xl border-lightGrey ">
-            <p className='mb-0 text-xl text-sm md:text-lg'>Payout per ticket: <span className='font-bold text-green'>£{ogPayout}</span></p>
+            <p className='mb-0 text-xl md:text-lg'>Payout per ticket: <span className='font-bold text-green'>£{ogPayout}</span></p>
             <span className='hidden md:flex mx-2'> / </span> 
-            <p className='mb-1 md:mb-0 text-xl text-sm md:text-lg'>Profit per ticket: <span className={`font-bold text-green ${profitPerTicket < 0 ? 'text-red-500' : 'text-green'}`}>£{profitPerTicket}</span></p>
+            <p className='mb-1 md:mb-0 text-xl md:text-lg'>Profit per ticket: <span className={`font-bold text-green ${profitPerTicket < 0 ? 'text-red-500' : 'text-green'}`}>£{profitPerTicket}</span></p>
           </div>
           <div className="flex flex-col md:flex-row items-center border justify-center w-full p-3 rounded-xl border-lightGrey">
-            <p className='mb-0 text-xl text-sm md:text-lg'>Total payout: <span className='font-bold text-green'>£{payout}</span></p>
+            <p className='mb-0 text-xl md:text-lg'>Total payout: <span className='font-bold text-green'>£{payout}</span></p>
             <span className='hidden md:flex mx-2'> / </span> 
-            <p className='mb-1 md:mb-0 text-xl text-sm md:text-lg'> Total profit: <span className={`font-bold text-green ${profitPerTicket < 0 ? 'text-red-500' : 'text-green'}`}>£{profitTotal}</span></p>
+            <p className='mb-1 md:mb-0 text-xl md:text-lg'> Total profit: <span className={`font-bold text-green ${profitPerTicket < 0 ? 'text-red-500' : 'text-green'}`}>£{profitTotal}</span></p>
           </div>
         </div>
         <div className='pt-7 md:pt-8 pb-3 flex flex-col items-center text-xs md:text-md'>
           <p><b>Payout:</b> Seller listing -VGG fee (10%) & VAT (20% of fee)</p>
           <p><b>VGG price:</b> Seller listing +18% of list price</p>
-          <span className='mt-3 flex text-center text-lightGrey'>lastgoodsleep#1106</span>
+          <span className='mt-3 flex text-center text-lightGrey'>greatergood</span>
         </div>
       </div>
     </div>
